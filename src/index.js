2760c5effcc171837817175ccbf4c78dc1b2672b
Billy Janitsch <billy@kensho.com>
2017-01-21 11:14:58 -0500

    Add babel-plugin-lodash
    Can be disabled by passing {lodash: false} as an option.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+import pluginLodash from 'babel-plugin-lodash';
 import pluginReactRequire from 'babel-plugin-react-require';
 import presetLatest from 'babel-preset-latest';
 import presetStage1 from 'babel-preset-stage-1';
-import react from 'babel-preset-react';
+import presetReact from 'babel-preset-react';
 
-export default () => {
+const defaultOptions = {
+  lodash: true,
+}
+
+export default (context, options) => {
+  options = {...defaultOptions, ...options}
   return {
     plugins: [
+      options.lodash && pluginLodash,
       pluginReactRequire,
-    ],
+    ].filter(Boolean),
     presets: [
       presetLatest,
       presetStage1,
