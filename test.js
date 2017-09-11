57ec39e8d6aa9e54baeb011d58b2073075ecdfc8
Billy Janitsch <billy@kensho.com>
2017-09-11 15:52:27 -0400

    Bump dependencies

--- test.js
+++ test.js
@@ -9,39 +9,29 @@ const macro = (t, input, regexes, presetOptions = {}) => {
   regexes.forEach(regex => { t.regex(code, regex) })
 }
 
-test('transforms ES2015 imports/exports', macro, `
-  import foo from 'foo'
-  export default 5
-`, [
-  /require\('foo'\)/,
-  /exports.default = 5/,
-])
+test('transforms ES2015 imports/exports', macro,
+  'import foo from \'foo\'; export default 5',
+  [/require\('foo'\)/, /exports.default = 5/]
+)
 
-test('cherry picks lodash modules', macro, `
-  import {max, range} from 'lodash'
-  module.exports = max(range(5))
-`, [
-  /require\('lodash\/max'\)/,
-  /require\('lodash\/range'\)/,
-])
+test('cherry picks lodash modules', macro,
+  'import {max, range} from \'lodash\'; module.exports = max(range(5))',
+  [/require\('lodash\/max'\)/, /require\('lodash\/range'\)/]
+)
 
-test('does not cherry pick lodash modules when {lodash: false}', macro, `
-  import {max, range} from 'lodash'
-  module.exports = max(range(5))
-`, [
-  /require\('lodash'\)/,
-], {lodash: false})
+test('does not cherry pick lodash modules when {lodash: false}', macro,
+  'import {max, range} from \'lodash\'; module.exports = max(range(5))',
+  [/require\('lodash'\)/],
+  {lodash: false}
+)
 
-test('requires React when JSX is present', macro, `
-  export default <div />
-`, [
-  /require\(['"]react['"]\)/,
-])
+test('requires React when JSX is present', macro,
+  'export default <div />',
+  [/require\(['"]react['"]\)/]
+)
 
-test('does not transpile imports/exports when {modules: false}', macro, `
-  import foo from 'foo'
-  export default foo
-`, [
-  /import foo from 'foo'/,
-  /export default foo/,
-], {modules: false})
+test('does not transpile imports/exports when {modules: false}', macro,
+  'import foo from \'foo\'; export default foo',
+  [/import foo from 'foo'/, /export default foo/],
+  {modules: false}
+)
