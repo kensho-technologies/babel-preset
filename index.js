77a17f0e10cdc60310dc17f799bd407cd800df73
Billy Janitsch <billy@kensho.com>
2018-02-15 21:27:35 -0800

    Add options for runtime transform and built-in usage

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
+const pluginLodash = require('babel-plugin-lodash')
+const pluginReactRequire = require('babel-plugin-react-require').default
+const pluginRuntime = require('@babel/plugin-transform-runtime')
+const presetEnv = require('@babel/preset-env')
+const presetStage1 = require('@babel/preset-stage-1')
+const presetReact = require('@babel/preset-react')
+
 const {BABEL_ENV, NODE_ENV} = process.env
 
 const defaultOptions = {
   lodash: true,
   loose: false,
   modules: BABEL_ENV === 'cjs' || NODE_ENV === 'test' ? 'commonjs' : false,
+  runtime: false,
   targets: {
     browsers: ['IE 11', 'last 2 Edge versions', 'Firefox >= 45', 'last 2 Chrome versions'],
     node: 8,
   },
+  useBuiltIns: true,
 }
 
 module.exports = (context, userOptions) => {
-  const {lodash, loose, modules, targets} = Object.assign({}, defaultOptions, userOptions)
+  const options = Object.assign({}, defaultOptions, userOptions)
+  const {lodash, loose, modules, runtime, targets, useBuiltIns} = options
 
-  const plugins = [require('babel-plugin-react-require').default]
-  if (lodash) plugins.push(require('babel-plugin-lodash'))
+  const plugins = [pluginReactRequire]
+  if (lodash) plugins.push(pluginLodash)
+  if (runtime) plugins.push([pluginRuntime, {polyfill: false, useBuiltIns}])
 
   const presets = [
-    [require('@babel/preset-env'), {loose, modules, targets}],
-    [require('@babel/preset-stage-1'), {loose, useBuiltIns: true}],
-    [require('@babel/preset-react'), {useBuiltIns: true}],
+    [presetEnv, {loose, modules, targets}],
+    [presetStage1, {loose, useBuiltIns}],
+    [presetReact, {useBuiltIns}],
   ]
 
   return {plugins, presets}
