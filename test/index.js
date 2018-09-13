7881c93f026158baeb1979184bc9880329dd7868
Billy Janitsch <billy@kensho.com>
2018-09-13 18:24:56 -0400

    Refactor test suite

--- test/index.js
+++ test/index.js
@@ -3,31 +3,39 @@ import fs from 'fs'
 import test from 'ava'
 import {transform} from '@babel/core'
 
+import preset from '..'
+
 function snapshot(t, fixture, presetOptions = {}, envName = 'development') {
   const filename = `${__dirname}/fixtures/${fixture}.js`
   const input = fs.readFileSync(filename, 'utf8')
-  const preset = require('..')
   const options = {envName, presets: [[preset, presetOptions]], babelrc: false}
   const result = transform(input, options)
   t.snapshot(result.code)
 }
 
+snapshot.title = (provided, fixture, options, envName) => {
+  const when = options ? `when ${JSON.stringify(options)}` : 'by default'
+  const inEnv = envName ? ` in ${envName} environment` : ''
+  return `${provided} ${when}${inEnv}`
+}
+
 test('transpiles ES2018+ syntax', snapshot, 'esnext')
-test('transpiles ES2018+ syntax in test env', snapshot, 'esnext', undefined, 'test')
-test(`transpiles ES2018+ syntax strictly when {loose: false}`, snapshot, 'esnext', {loose: false})
+test('transpiles ES2018+ syntax for node', snapshot, 'esnext', undefined, 'test')
+test('transpiles ES2018+ syntax strictly', snapshot, 'esnext', {loose: false})
 
 test('does not transpile ES modules', snapshot, 'esm')
-test(`transpiles ES modules when {modules: 'commonjs'}`, snapshot, 'esm', {modules: 'commonjs'})
-test(`transpiles ES modules in test env`, snapshot, 'esm', undefined, 'test')
+test('transpiles ES modules', snapshot, 'esm', {modules: 'commonjs'})
+test('transpiles ES modules', snapshot, 'esm', undefined, 'test')
 
-test('does not use esm babel helpers when {modules: commonjs}', snapshot, 'esm-helpers', {
-  modules: 'commonjs',
-  runtime: true,
-})
-test('uses esm babel helpers when {modules: false}', snapshot, 'esm-helpers', {runtime: true})
+test('does not use external helpers', snapshot, 'esm-helpers')
+test('uses external ESM helpers', snapshot, 'esm-helpers', {runtime: true})
+test('uses external CJS helpers', snapshot, 'esm-helpers', {modules: 'commonjs', runtime: true})
 
 test('cherry picks lodash modules', snapshot, 'lodash')
-test('does not modify lodash imports when {lodash: false}', snapshot, 'lodash', {lodash: false})
+test('does not cherry pick lodash modules', snapshot, 'lodash', {lodash: false})
 
 test('wraps prop types', snapshot, 'prop-types')
-test('removes prop types in production env', snapshot, 'prop-types', undefined, 'production')
+test('wraps prop types', snapshot, 'prop-types', {removePropTypes: 'wrap'})
+test('removes prop types', snapshot, 'prop-types', undefined, 'production')
+test('removes prop types', snapshot, 'prop-types', {removePropTypes: 'remove'})
+test('does not remove prop types', snapshot, 'prop-types', {removePropTypes: false})
