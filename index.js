22602d437e579c40759be750de6d6af3c44ad31f
Billy Janitsch <billy@kensho.com>
2019-07-04 13:12:06 -0400

    Upgrade dependencies

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ module.exports = (babel, options) => {
   const overrides = [
     {
       exclude: /node_modules/,
-      plugins: [
-        [require('@babel/plugin-proposal-class-properties').default, {loose}],
-        [require('@babel/plugin-syntax-dynamic-import').default],
-      ],
+      plugins: [[require('@babel/plugin-proposal-class-properties').default, {loose}]],
       presets: [
         typescript && [require('@babel/preset-typescript').default, typescript],
         react && [
