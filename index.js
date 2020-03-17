7dac1f6de87e069545df623fec351ddde864ab2b
Billy Janitsch <billy@kensho.com>
2020-03-17 17:53:41 -0400

    Blacklist some known compiled packages (#86)
    * Blacklist some known compiled packages
    
    * Avoid optimizing styling of pre-compiled packages

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /* eslint-disable global-require */
 
+const NODE_MODULES_REGEX = /node_modules/
+
+const PRECOMPILED_PACKAGES = ['core-js', 'lodash', 'react', 'react-dom', 'whatwg-fetch']
+const PRECOMPILED_PACKAGES_REGEX = new RegExp(`node_modules/(${PRECOMPILED_PACKAGES.join('|')})/`)
+
 module.exports = (babel, options) => {
   const env = babel.env()
   const {
@@ -13,44 +18,45 @@ module.exports = (babel, options) => {
   } = options
   const {reactRefresh = env === 'development' && react && {}} = options
 
-  const plugins = [
-    runtime && [
-      require('@babel/plugin-transform-runtime').default,
-      {useESModules: !modules, version: require('@babel/runtime/package.json').version},
+  const nodeModules = {
+    include: NODE_MODULES_REGEX,
+    compact: true,
+  }
+
+  const nonPrecompiledPackages = {
+    exclude: PRECOMPILED_PACKAGES_REGEX,
+    plugins: [
+      runtime && [
+        require('@babel/plugin-transform-runtime').default,
+        {useESModules: !modules, version: require('@babel/runtime/package.json').version},
+      ],
+    ].filter(Boolean),
+    presets: [
+      [
+        require('@babel/preset-env').default,
+        {loose, modules, targets, corejs: 3, useBuiltIns: 'entry'},
+      ],
     ],
-  ].filter(Boolean)
+  }
 
-  const presets = [
-    [
-      require('@babel/preset-env').default,
-      {loose, modules, targets, corejs: 3, useBuiltIns: 'entry'},
-    ],
-  ]
+  const nonNodeModules = {
+    exclude: NODE_MODULES_REGEX,
+    plugins: [
+      [require('@babel/plugin-proposal-class-properties').default, {loose}],
+      reactRefresh && [require('react-refresh/babel'), {skipEnvCheck: true, ...reactRefresh}],
+    ].filter(Boolean),
+    presets: [
+      typescript && [require('@babel/preset-typescript').default, typescript],
+      react && [
+        require('@babel/preset-react').default,
+        {development: env === 'development', useSpread: true, ...react},
+      ],
+      emotion && [
+        require('@emotion/babel-preset-css-prop').default,
+        {autoLabel: env === 'development', sourceMap: env === 'development', ...emotion},
+      ],
+    ].filter(Boolean),
+  }
 
-  const overrides = [
-    {
-      include: /node_modules/,
-      compact: true,
-    },
-    {
-      exclude: /node_modules/,
-      plugins: [
-        [require('@babel/plugin-proposal-class-properties').default, {loose}],
-        reactRefresh && [require('react-refresh/babel'), {skipEnvCheck: true, ...reactRefresh}],
-      ].filter(Boolean),
-      presets: [
-        typescript && [require('@babel/preset-typescript').default, typescript],
-        react && [
-          require('@babel/preset-react').default,
-          {development: env === 'development', useSpread: true, ...react},
-        ],
-        emotion && [
-          require('@emotion/babel-preset-css-prop').default,
-          {autoLabel: env === 'development', sourceMap: env === 'development', ...emotion},
-        ],
-      ].filter(Boolean),
-    },
-  ]
-
-  return {plugins, presets, overrides}
+  return {overrides: [nodeModules, nonPrecompiledPackages, nonNodeModules]}
 }
--- test.js
+++ test.js
@@ -28,7 +28,8 @@ test('transpiles ES2018+ syntax for specific targets', snapshot, 'esnext.js', {
 })
 test('transpiles ES2018+ syntax strictly', snapshot, 'esnext.js', {loose: false})
 
-test('transpiles standard JS in node_modules', snapshot, 'node_modules/dependency.js')
+test('transpiles standard JS in node_modules', snapshot, 'node_modules/my-pkg/index.js')
+test('avoids transpiling known precompiled packages', snapshot, 'node_modules/react/index.js')
 
 test('does not transpile ES modules', snapshot, 'esm.js')
 test('transpiles ES modules', snapshot, 'esm.js', {modules: 'commonjs'})
