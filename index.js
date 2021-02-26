658c71c516296dc388119db39145c2d70344707e
Billy Janitsch <billy@kensho.com>
2021-02-26 15:19:50 -0500

    Upgrade dependencies and use conditional export runtime helpers

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ module.exports = (babel, options) => {
     plugins: [
       runtime && [
         require('@babel/plugin-transform-runtime').default,
-        {useESModules: !modules, version: require('@babel/runtime/package.json').version},
+        {version: require('@babel/runtime/package.json').version},
       ],
     ].filter(Boolean),
     presets: [
