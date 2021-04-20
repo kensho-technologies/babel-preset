aa70ac1fd7eccf23e1d7f40ca405e2a48d7a2306
Nikita <nikita.kiriy@kensho.com>
2021-04-20 14:07:55 -0400

    add async/await and generators to syntax tests

--- fixtures/syntax.js
+++ fixtures/syntax.js
@@ -4,8 +4,22 @@ import * as namespaceImport from 'bar'
 const dynamicImport = import('baz')
 
 class Foo {
+  *methodGenerator(seed) {
+    yield seed
+  }
   static bar = 'abc'
   baz = (x, y) => x({...y})
+}
+
+function* generator() {
+  const input = yield 'using last argument passed to next()'
+  yield* new Foo().methodGenerator(input)
+}
+
+const promise = new Promise((resolve) => resolve())
+
+async function asyncFunction() {
+  return await Promise.all([promise])
 }
 
 const obj = {a: 1, b: 2}
